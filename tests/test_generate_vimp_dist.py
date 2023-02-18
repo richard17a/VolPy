0ca83ab6776ea7967db1528f862629e4bcfd2a59
Richard Anslow <r.anslow@outlook.com>
2023-02-18 23:27:45 +0000

    added the remaining velocity impact distribution functions

--- tests/test_generate_vimp_dist.py
+++ tests/test_generate_vimp_dist.py
@@ -2,66 +2,134 @@
 Docstring
 """
 
-from volpy.velocities.generate_vimp_dist import calculate_relative_velocity
+from volpy.velocities.generate_vimp_dist import calculate_relative_velocity, calculate_escape_velocity, generate_vimp_dist
 import numpy as np
 import pytest
 
 
 def test_calculate_relative_velocity():
+    """
+    Docstring
+    """
 
-	v_rel_299 = calculate_relative_velocity(tiss=2.99,
-										M_star=1.,
-										a_pl=1.,
-										G=6.67e-11,
-										AU=1.496e11,
-										M_solar=1.989e30)
-	v_rel_3 = calculate_relative_velocity(tiss=3.,
-										M_star=1.,
-										a_pl=1.,
-										G=6.67e-11,
-										AU=1.496e11,
-										M_solar=1.989e30)
+    v_rel_299 = calculate_relative_velocity(tiss=2.99,
+                                        M_star=1.,
+                                        a_pl=1.,
+                                        G=6.67e-11,
+                                        AU=1.496e11,
+                                        M_solar=1.989e30)
+    v_rel_3 = calculate_relative_velocity(tiss=3.,
+                                        M_star=1.,
+                                        a_pl=1.,
+                                        G=6.67e-11,
+                                        AU=1.496e11,
+                                        M_solar=1.989e30)
 
-	with pytest.raises(TypeError) as e_info:
-		v_rel = calculate_relative_velocity(tiss=np.linspace(2., 3., 100),
-											M_star=1.,
-											a_pl=1.,
-											G=6.67e-11,
-											AU=1.496e11,
-											M_solar=1.989e30)
+    with pytest.raises(TypeError) as e_info:
+        v_rel = calculate_relative_velocity(tiss=np.linspace(2., 3., 100),
+                                            M_star=1.,
+                                            a_pl=1.,
+                                            G=6.67e-11,
+                                            AU=1.496e11,
+                                            M_solar=1.989e30)
 
-	with pytest.raises(TypeError) as e_info:
-		v_rel = calculate_relative_velocity(tiss=2.99,
-											M_star=1,
-											a_pl=1.,
-											G=6.67e-11,
-											AU=1.496e11,
-											M_solar=1.989e30)
+    with pytest.raises(TypeError) as e_info:
+        v_rel = calculate_relative_velocity(tiss=2.99,
+                                            M_star=1,
+                                            a_pl=1.,
+                                            G=6.67e-11,
+                                            AU=1.496e11,
+                                            M_solar=1.989e30)
 
-	with pytest.raises(TypeError) as e_info:
-		v_rel = calculate_relative_velocity(tiss=2.99,
-											M_star=1.,
-											a_pl=1,
-											G=6.67e-11,
-											AU=1.496e11,
-											M_solar=1.989e30)
+    with pytest.raises(TypeError) as e_info:
+        v_rel = calculate_relative_velocity(tiss=2.99,
+                                            M_star=1.,
+                                            a_pl=1,
+                                            G=6.67e-11,
+                                            AU=1.496e11,
+                                            M_solar=1.989e30)
 
-	with pytest.raises(TypeError) as e_info:
-		v_rel = calculate_relative_velocity(tiss=2.99,
-											M_star=1.,
-											a_pl=1,
-											G=6.67e-11,
-											AU='AU',
-											M_solar=1.989e30)
+    with pytest.raises(TypeError) as e_info:
+        v_rel = calculate_relative_velocity(tiss=2.99,
+                                            M_star=1.,
+                                            a_pl=1,
+                                            G=6.67e-11,
+                                            AU='AU',
+                                            M_solar=1.989e30)
 
-	with pytest.raises(TypeError) as e_info:
-		v_rel = calculate_relative_velocity(tiss=2.99,
-											M_star=1,
-											a_pl=1.,
-											G=6.67e-11,
-											AU=1.496e11,
-											M_solar=1)
+    with pytest.raises(TypeError) as e_info:
+        v_rel = calculate_relative_velocity(tiss=2.99,
+                                            M_star=1,
+                                            a_pl=1.,
+                                            G=6.67e-11,
+                                            AU=1.496e11,
+                                            M_solar=1)
 
-	assert v_rel_299, v_rel_3
-	assert np.isclose(v_rel_299 / 1e3, 29.779 * np.sqrt(3 - 2.99), rtol=1e-4)
-	assert v_rel_3 == 0
+    assert v_rel_299, v_rel_3
+    assert np.isclose(v_rel_299 / 1e3, 29.779 * np.sqrt(3 - 2.99), rtol=1e-4)
+    assert v_rel_3 == 0
+
+
+def test_calculate_escape_velocity():
+    """
+    Docstring
+    """
+
+    v_esc_earth = calculate_escape_velocity(M_pl=3.00273e-6,
+                                            R_pl=4.2635e-5,
+                                            G=6.67e-11,
+                                            AU=1.496e11,
+                                            M_solar=1.989e30)
+    v_esc_mars = calculate_escape_velocity(M_pl=3.00273e-6 * 0.107,
+                                            R_pl=4.2635e-5 * 0.53,
+                                            G=6.67e-11,
+                                            AU=1.496e11,
+                                            M_solar=1.989e30)
+
+    assert v_esc_earth
+    assert np.isclose(v_esc_earth / 1e3, 11.176, rtol=1e-4)
+    assert v_esc_mars < v_esc_earth
+
+
+def test_generate_vimp_dist():
+    """
+    Docstring
+    """
+
+    with pytest.raises(TypeError) as e_info:
+        v_imp = generate_vimp_dist(tiss_params=[1, 2, 3, 4],
+                                   M_star=1,
+                                   M_pl=3.00e-6,
+                                   R_pl=4.26e-5,
+                                   a_pl=1.,
+                                   G=6.67e-11,
+                                   AU=1.496e11,
+                                   M_solar=1.989e30)
+
+    v_imp = generate_vimp_dist(tiss_params=np.array([3.]),
+                               M_star=1.,
+                               M_pl=3.00e-6,
+                               R_pl=4.26e-5,
+                               a_pl=1.,
+                               G=6.67e-11,
+                               AU=1.496e11,
+                               M_solar=1.989e30)
+    v_esc = calculate_escape_velocity(M_pl=3.00e-6,
+                                      R_pl=4.26e-5,
+                                      G=6.67e-11,
+                                      AU=1.496e11,
+                                      M_solar=1.989e30)
+
+    tiss_params = np.linspace(2., 2.999, 1000)
+    v_imp_2 = generate_vimp_dist(tiss_params=tiss_params,
+                                 M_star=1.,
+                                 M_pl=3.00e-6,
+                                 R_pl=4.26e-5,
+                                 a_pl=1.,
+                                 G=6.67e-11,
+                                 AU=1.496e11,
+                                 M_solar=1.989e30)
+
+    assert v_imp
+    assert v_imp[0] == v_esc
+    assert len(v_imp_2) == len(tiss_params)
--- volpy/velocities/generate_vimp_dist.py
+++ volpy/velocities/generate_vimp_dist.py
@@ -6,32 +6,104 @@ import numpy as np
 
 
 def calculate_relative_velocity(tiss: float,
-								M_star: float, 
-								a_pl: float, 
-								G: float, 
-								AU: float, 
-								M_solar: float):
-	"""
-	Docstring
+                                M_star: float, 
+                                a_pl: float, 
+                                G: float, 
+                                AU: float, 
+                                M_solar: float):
+    """
+    Docstring
 
-	v_rel = v_kep * sqrt(3 - Tiss)
+    v_rel = v_kep * sqrt(3 - Tiss)
 
-	"""
+    """
 
-	if not isinstance(tiss, float):
-		raise TypeError
-	if not isinstance(M_star, float):
-		raise TypeError
-	if not isinstance(a_pl, float):
-		raise TypeError
-	if not isinstance(G, float):
-		raise TypeError
-	if not isinstance(AU, float):
-		raise TypeError
-	if not isinstance(M_solar, float):
-		raise TypeError
+    if not isinstance(tiss, float):
+        raise TypeError
+    if not isinstance(M_star, float):
+        raise TypeError
+    if not isinstance(a_pl, float):
+        raise TypeError
+    if not isinstance(G, float):
+        raise TypeError
+    if not isinstance(AU, float):
+        raise TypeError
+    if not isinstance(M_solar, float):
+        raise TypeError
 
-	v_kep = np.sqrt((G * M_star * M_solar) / (a_pl * AU))
-	v_rel = v_kep * np.sqrt(3 - tiss)
+    v_kep = np.sqrt((G * M_star * M_solar) / (a_pl * AU))
+    v_rel = v_kep * np.sqrt(3 - tiss)
 
-	return v_rel
+    return v_rel
+
+
+def calculate_escape_velocity(M_pl: float, 
+                              R_pl: float,
+                              G: float,
+                              AU: float, 
+                              M_solar: float):
+    """
+    Docstring
+    """
+    if not isinstance(M_pl, float):
+        raise TypeError
+    if not isinstance(R_pl, float):
+        raise TypeError
+    if not isinstance(AU, float):
+        raise TypeError
+    if not isinstance(M_solar, float):
+        raise TypeError
+
+    v_esc = np.sqrt( (2 * G * M_pl * M_solar) / (R_pl * AU))
+
+    return v_esc
+
+
+def generate_vimp_dist(tiss_params: np.ndarray,
+                       M_star: float, 
+                       M_pl: float, 
+                       R_pl: float, 
+                       a_pl: float, 
+                       AU: float,
+                       G: float,
+                       M_solar: float):
+    """
+    Docstring
+    """
+    if not isinstance(tiss_params, np.ndarray):
+        raise TypeError
+    if not isinstance(M_star, float):
+        raise TypeError
+    if not isinstance(M_pl, float):
+        raise TypeError
+    if not isinstance(R_pl, float):
+        raise TypeError
+    if not isinstance(a_pl, float):
+        raise TypeError
+    if not isinstance(G, float):
+        raise TypeError
+    if not isinstance(AU, float):
+        raise TypeError
+    if not isinstance(M_solar, float):
+        raise TypeError
+
+    v_esc = calculate_escape_velocity(M_pl=M_pl,
+                                      R_pl=R_pl,
+                                      G=G,
+                                      AU=AU,
+                                      M_solar=M_solar)
+
+    v_imp = []
+
+    for tiss in tiss_params:
+
+        v_rel = calculate_relative_velocity(tiss=tiss,
+                                            M_star=M_star,
+                                            a_pl=a_pl,
+                                            G=G,
+                                            AU=AU,
+                                            M_solar=M_solar)
+
+        v_imp = np.append(v_imp, np.sqrt(v_esc ** 2 + v_rel ** 2))
+
+    return v_imp
