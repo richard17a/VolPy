1fe3e6d95d8daae8e628bfa2e8da79fa5a608446
Richard Anslow <r.anslow@outlook.com>
2023-02-18 23:51:11 +0000

    cleaned up lint warnings

--- volpy/velocities/generate_vimp_dist.py
+++ volpy/velocities/generate_vimp_dist.py
@@ -2,15 +2,15 @@
 Docstring
 """
 
-import numpy as np 
+import numpy as np
 
 
 def calculate_relative_velocity(tiss: float,
-                                M_star: float, 
-                                a_pl: float, 
-                                G: float, 
-                                AU: float, 
-                                M_solar: float):
+                                m_star: float,
+                                a_pl: float,
+                                grav_const: float,
+                                au_: float,
+                                m_solar: float):
     """
     Docstring
 
@@ -20,89 +20,91 @@ def calculate_relative_velocity(tiss: float,
 
     if not isinstance(tiss, float):
         raise TypeError
-    if not isinstance(M_star, float):
+    if not isinstance(m_star, float):
         raise TypeError
     if not isinstance(a_pl, float):
         raise TypeError
-    if not isinstance(G, float):
+    if not isinstance(grav_const, float):
         raise TypeError
-    if not isinstance(AU, float):
+    if not isinstance(au_, float):
         raise TypeError
-    if not isinstance(M_solar, float):
+    if not isinstance(m_solar, float):
         raise TypeError
 
-    v_kep = np.sqrt((G * M_star * M_solar) / (a_pl * AU))
+    v_kep = np.sqrt((grav_const * m_star * m_solar) / (a_pl * au_))
     v_rel = v_kep * np.sqrt(3 - tiss)
 
     return v_rel
 
 
-def calculate_escape_velocity(M_pl: float, 
-                              R_pl: float,
-                              G: float,
-                              AU: float, 
-                              M_solar: float):
+def calculate_escape_velocity(m_pl: float,
+                              r_pl: float,
+                              grav_const: float,
+                              au_: float,
+                              m_solar: float):
     """
     Docstring
     """
-    if not isinstance(M_pl, float):
+    if not isinstance(m_pl, float):
         raise TypeError
-    if not isinstance(R_pl, float):
+    if not isinstance(r_pl, float):
         raise TypeError
-    if not isinstance(AU, float):
+    if not isinstance(grav_const, float):
         raise TypeError
-    if not isinstance(M_solar, float):
+    if not isinstance(au_, float):
+        raise TypeError
+    if not isinstance(m_solar, float):
         raise TypeError
 
-    v_esc = np.sqrt( (2 * G * M_pl * M_solar) / (R_pl * AU))
+    v_esc = np.sqrt( (2 * grav_const * m_pl * m_solar) / (r_pl * au_))
 
     return v_esc
 
 
 def generate_vimp_dist(tiss_params: np.ndarray,
-                       M_star: float, 
-                       M_pl: float, 
-                       R_pl: float, 
-                       a_pl: float, 
-                       AU: float,
-                       G: float,
-                       M_solar: float):
+                       m_star: float,
+                       m_pl: float,
+                       r_pl: float,
+                       a_pl: float,
+                       au_: float,
+                       grav_const: float,
+                       m_solar: float):
     """
     Docstring
     """
     if not isinstance(tiss_params, np.ndarray):
         raise TypeError
-    if not isinstance(M_star, float):
+    if not isinstance(m_star, float):
         raise TypeError
-    if not isinstance(M_pl, float):
+    if not isinstance(m_pl, float):
         raise TypeError
-    if not isinstance(R_pl, float):
+    if not isinstance(r_pl, float):
         raise TypeError
     if not isinstance(a_pl, float):
         raise TypeError
-    if not isinstance(G, float):
+    if not isinstance(grav_const, float):
         raise TypeError
-    if not isinstance(AU, float):
+    if not isinstance(au_, float):
         raise TypeError
-    if not isinstance(M_solar, float):
+    if not isinstance(m_solar, float):
         raise TypeError
 
-    v_esc = calculate_escape_velocity(M_pl=M_pl,
-                                      R_pl=R_pl,
-                                      G=G,
-                                      AU=AU,
-                                      M_solar=M_solar)
+    v_esc = calculate_escape_velocity(m_pl=m_pl,
+                                      r_pl=r_pl,
+                                      grav_const=grav_const,
+                                      au_=au_,
+                                      m_solar=m_solar)
 
     v_imp = []
 
     for tiss in tiss_params:
 
         v_rel = calculate_relative_velocity(tiss=tiss,
-                                            M_star=M_star,
+                                            m_star=m_star,
                                             a_pl=a_pl,
-                                            G=G,
-                                            AU=AU,
-                                            M_solar=M_solar)
+                                            grav_const=grav_const,
+                                            au_=au_,
+                                            m_solar=m_solar)
 
         v_imp = np.append(v_imp, np.sqrt(v_esc ** 2 + v_rel ** 2))
 
